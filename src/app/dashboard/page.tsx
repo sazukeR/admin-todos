38f00ccf956b7791870157d467f6b5e25359a72a
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-11-24 12:39:46 -0500

    Add auth session page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,27 @@
 import { WidgetItem } from "@/components/WidgetItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+ const session = await getServerSession(authOptions);
+ 7;
+
+ if (!session) {
+  redirect("/api/auth/signin");
+ }
+
  return (
   // <div className="justify-around flex flex-wrap bg-gray-400 min-h-screen">
 
-  <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-   {/* <WidgetItem />
-   <WidgetItem />
-   <WidgetItem />
-   <WidgetItem />
-   <WidgetItem />
-   <WidgetItem /> */}
+  <div className="grid gap-6 grid-cols-1 md:grid-cols-2">
+   <WidgetItem title={"Connected user Server-Side"}>
+    <div className="grid grid-cols-1">
+     <span>{session?.user?.name}</span>
+     <span>{session?.user?.image}</span>
+     <span>{session?.user?.email}</span>
+    </div>
+   </WidgetItem>
   </div>
  );
 }
