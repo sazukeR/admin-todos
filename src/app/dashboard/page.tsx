575a3a0ed656f93712386890173978005dc974fb
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-11-26 11:36:03 -0500

    Add google auth provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,16 @@
-import { WidgetItem } from "@/components/WidgetItem";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { WidgetItem } from "@/components/WidgetItem";
 
 export default async function DashboardPage() {
  const session = await getServerSession(authOptions);
- 7;
 
  if (!session) {
   redirect("/api/auth/signin");
  }
 
  return (
-  // <div className="justify-around flex flex-wrap bg-gray-400 min-h-screen">
-
   <div className="grid gap-6 grid-cols-1 md:grid-cols-2">
    <WidgetItem title={"Connected user Server-Side"}>
     <div className="grid grid-cols-1">
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import { CiLogout } from "react-icons/ci";
@@ -11,7 +9,11 @@ import {
  IoCalendarOutline,
  IoCheckboxOutline,
  IoListOutline,
+ IoPersonOutline,
 } from "react-icons/io5";
+import { getServerSession, Session } from "next-auth";
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const menuItems = [
  {
@@ -39,9 +41,22 @@ const menuItems = [
   path: "/dashboard/products",
   title: "Products",
  },
+ {
+  icon: <IoPersonOutline />,
+  path: "/dashboard/profile",
+  title: "Profile",
+ },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+ const session = await getServerSession(authOptions);
+
+ const userUrl = session?.user?.image
+  ? session?.user?.image
+  : "https://img.freepik.com/vector-gratis/ilustracion-icono-perfil_53876-5907.jpg";
+
+ const userName = session?.user?.name ? session?.user?.name : "No name";
+
  return (
   <aside className=" ml-[-100%] fixed z-10 top-0 pb-3 px-3 w-full flex flex-col justify-between h-screen border-r border-gray-200 bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
    <div className="bg-red-300">
@@ -62,13 +77,13 @@ export const Sidebar = () => {
      <Image
       width={100}
       height={100}
-      src="https://img.freepik.com/vector-gratis/ilustracion-icono-perfil_53876-5907.jpg"
+      src={userUrl}
       alt="User avatar"
       className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
       unoptimized
      />
      <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-      Cynthia J. Watts
+      {userName}
      </h5>
      <span className="hidden text-gray-400 lg:block">Admin</span>
     </div>
